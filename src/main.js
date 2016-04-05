f50dd43d082a9a3653fb4d5bf1399fc45ac73d31
Deena Wang <thedeenawang@gmail.com>
2016-04-05 09:32:08 -0400

    Minimal Babel transpiling with watch

---
+++ src/main.js
@@ -0,0 +1,18 @@
+class Foo { // this is ES6 syntax!
+  constructor(msg) {
+    this.msg = msg;
+  }
+  speak() {
+    console.log(this.msg);
+  }
+}
+
+console.log('hello world');
+
+ReactDOM.render(
+  <h1>Hello world, React!</h1>, // this is React's JSX syntax!
+  document.getElementById('example')
+);
+
+var f = new Foo('Hi there, console!');
+f.speak()
